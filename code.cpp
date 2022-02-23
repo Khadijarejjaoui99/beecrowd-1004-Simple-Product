c92e61a190089db373ee07bafcad8ec2ed854fe2
FIRST_NAME LAST_NAME <khadijarejjaoui41@gmail.com>
2022-02-23 20:45:00 +0600

    first commit

---
+++ code.cpp
@@ -0,0 +1,14 @@
+// Solution of 1004 - Simple Product in beecrowd
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+ 
+    int A,B,PROD;
+    cin >> A >>B;
+    PROD  = A * B;
+    cout<<"PROD = " << PROD  <<endl;
+    system("pause");
+    return 0;
+}
